4ead9ddd721aab05c92a68e129abdee2d33f59d2
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2022-12-03 16:14:03 +0300

    Ex 005 сделали тру фолс

---
+++ Example005IFelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Ура это Даша!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
