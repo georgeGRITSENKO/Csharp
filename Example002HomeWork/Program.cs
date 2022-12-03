1ec951a9bf4086c52570bbf8e405023ff4e35b2a
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2022-12-03 18:41:09 +0300

    Ex 2 HOMEWORK

---
+++ Example002HomeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" Hello, It's second exercise of homework !");
+Console.WriteLine(" Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+Console.WriteLine(" 4 -> да");
+Console.WriteLine(" -3 -> нет");
+Console.WriteLine(" 7 -> нет");
+
+int a = 4;
+int b = -3;
+int c = 7;
+
+if ((a%2) == 0)
+{
+     Console.WriteLine("YES");
+} 
+else 
+{
+    Console.WriteLine("NO");
+}
+if ((b%2) == 0)
+{
+     Console.WriteLine("YES");
+} 
+else 
+{
+    Console.WriteLine("NO");
+}
+if ((c%2) == 0)
+{
+     Console.WriteLine("YES");
+} 
+else 
+{
+    Console.WriteLine("NO");
+}
+ 
+
