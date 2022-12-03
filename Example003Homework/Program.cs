fbf350dafe091c5a9a2e0800ce529ddf96dc101d
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2022-12-03 19:02:07 +0300

    Ex 3 HomeWork

---
+++ Example003Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 5;
+int b = 8;
+int countA = 1;
+while(countA<a+1)
+{
+    if((countA % 2) == 0)
+    {
+        Console.Write(countA);
+        Console.Write(",");
+    }
+    countA++;
+}
+Console.Write(".");
+Console.WriteLine("");
+int countB = 1;
+while(countB<=b+1)
+{
+    if((countB % 2) == 0)
+    {
+        Console.Write(countB);
+        Console.Write(",");
+    }
+    countB++;
+}
+Console.Write(".");
+Console.WriteLine("");
