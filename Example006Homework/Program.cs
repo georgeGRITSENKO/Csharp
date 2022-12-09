5f555334e59bb122acc9810c25782d137f6ff0f0
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2022-12-10 01:04:53 +0300

    3 задача 2 дз

---
+++ Example006Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Ввидите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number == 6  || Number == 7){
+
+  Console.WriteLine("Да,сегодня выходной");
+
+}
+
+else if (Number < 1 || Number >7) {
+
+  Console.WriteLine("Извините, в неделе 7 дней ");
+
+}
+else  {
+
+  Console.WriteLine("Нет, сегодня рабочий день (не выходной)");
+
+}
+
+
