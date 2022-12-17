c0aa5bf7c27cce1d4ce4c43aba5f779d50fde965
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2022-12-17 12:33:29 +0300

    ДЗ 4 / 3

---
+++ Example012HomeWork/Program.cs
@@ -0,0 +1,34 @@
+﻿int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(0,14);
+    Console.Write(randomArray[i]+ " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Pow(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+int TODEC(int[] value)
+{
+    int result = 0;
+    int count = value.Length;
+    for (int i = 0; i < value.Length; i++)
+{
+    result += value[i] * Pow(15, count - 1 - i);
+}
+    return result;
+}
+Console.WriteLine(TODEC(randomArray));
