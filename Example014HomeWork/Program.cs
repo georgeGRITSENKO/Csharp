c843b91e836f590f9918492367d39400f405b1f8
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2022-12-18 10:08:23 +0300

    ДЗ 5/2

---
+++ Example014HomeWork/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int sum = 0;
+for (int z = 1; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,100);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
